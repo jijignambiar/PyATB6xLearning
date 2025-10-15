37686e5eb1056cd2fd2a689e6266c1e31f27c43d
jijig <jijignambiar@gmail.com>
2025-10-15 23:10:10 +0530

    Version5

---
+++ src/Task/AreaTriangle.py
@@ -0,0 +1,11 @@
+# Python program to calculate the area of a triangle using the formula
+
+# Area = 1/2 *base*height
+# base =10, height =5
+
+base = float(input("enter base value of a triangle:"))
+height = float(input("enter height of a triangle :"))
+area = (1/2 * base * height)
+print("Base of the triangle:",base)
+print("Height of the triangle:",height)
+print("Area of the triangle:",area)
---
+++ src/Task/NumberGr100.py
@@ -0,0 +1,5 @@
+num = int(input("enter a number:"))
+if(num > 100) :
+    print("num is greater than 100 :" ,num)
+else :
+    print("num is less than 100 :" ,num)
